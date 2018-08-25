0d54b4295c294955473ec6ea37bc02686440bbda
Alexis Roda <alexis.roda.villalonga@gmail.com>
2018-08-26 00:51:35 +0200

    Eliminar dependència de "python-django".
    Es substitueix "python-django" per "emacs-djira". S'elimina codi
    innecessari i s'adapta la resta.
    
    No s'afegeix cap nova funcionalitat.

--- arv-py-django.el
+++ arv-py-django.el
@@ -40,7 +40,7 @@
 ;; Aquest mòdul integra diferents eines per facilitar el treball amb
 ;; django:
 ;;
-;; * python-django: ofereix una gestió de projecte
+;; * emacs-djira: introspecció d'un projecte django
 ;;
 ;; * pony-tpl: la part de pony-mode encarregada de les plantilles
 ;;
@@ -68,96 +68,10 @@
 ;;; Code:
 
 (require 'dash)
-(require 'python-django)
+(require 'f)
 (require 's)
 (require 'thingatpt)
 
-;; funcions de suport
-
-(defun arv/django--vector-to-list (v)
-  "Converteix recursivament un vector de vectors en una llista de llistes."
-  (interactive)
-  (mapcar (lambda (x)
-            (if (vectorp x)
-                (arv/django--vector-to-list x)
-              x))
-          v))
-
-(defun arv/django-open-default-project ()
-  "Obre el projecte django definit per les variables d'entorn
-`DJANGO_PROJECT' i `DJANGO_SETTINGS_MODULE', si estan
-definides. En cas contrari demana a l'usuari les dades.
-
-Si ja hi ha un projecte obert no té cap efecte.
-
-Retorna el buffer del projecte.
-
-La idea és que el hook `postactivate' de virtualenvwrapper
-defineixi aquestes variables amb el valor adient. Aquesta solució
-m'agrada més que utilitzar variables locals de directori."
-  (interactive)
-  (or (arv/django-get-project-buffer)
-      (if (getenv "DJANGO_PROJECT")
-          (python-django-open-project (getenv "DJANGO_PROJECT")
-                                      (getenv "DJANGO_SETTINGS_MODULE"))
-        (call-interactively 'python-django-open-project))))
-
-(defun arv/django-get-project-buffer ()
-  "Retorna el buffer del projecte o nil si no hi ha cap projecte
-obert. Assumeix que hi ha UN ÚNIC PROJECTE obert."
-  (-first (lambda (buf)
-            (with-current-buffer buf
-              (eq major-mode 'python-django-mode)))
-          (buffer-list)))
-
-(defun arv/django-file-belongs-to-app-p (filename)
-  "Determina si `filename' correspon a un arxiu dins alguna de
-les aplicacions del projecte. En cas afirmatiu retorna el nom de
-la aplicació, nil en cas contrari.
-
-L'ordre en que s'examinen les aplicacions no està definit."
-  (if filename
-      (let ((tpl-dir (file-name-directory (expand-file-name filename)))
-            (project-buffer (arv/django-get-project-buffer)))
-        (if project-buffer
-            (with-current-buffer project-buffer
-              (car
-               (-first
-                (lambda (app) (s-starts-with-p (cdr app) tpl-dir))
-                (python-django-info-get-app-paths))))))))
-
-(defun arv/django-get-current-app ()
-  "Retorna el nom de l'aplicació que conté el buffer
-actiu. Retorna nil si el buffer no pertany a cap aplicació o no
-s'ha carregat cap projecte django."
-  (arv/django-file-belongs-to-app-p buffer-file-name))
-
-(defun arv/django-get-app-names ()
-  ""
-  (with-current-buffer (arv/django-get-project-buffer)
-    (sort (python-django-info-get-installed-apps) 'string-lessp)))
-
-(defun arv/django--get-app-path (app-name)
-  ""
-    (with-current-buffer (arv/django-get-project-buffer)
-      (python-django-info-get-app-path app-name)))
-
-(defun arv/django-get-project-root ()
-  (with-current-buffer (arv/django-get-project-buffer)
-    python-django-project-root))
-
-;;;###autoload
-(defun arv/django-switch-to-project-buffer ()
-  "Canvia el focus a la finestra que te el projecte django. Si
-cap finestra mostra el projecte el mostra en la finestra actual,
-obrint-lo si cal."
-  (interactive)
-  (let* ((project-buffer (or (arv/django-get-project-buffer)
-                             (arv/django-open-default-project)))
-         (project-window (get-buffer-window project-buffer)))
-    (if project-window
-        (select-window project-window)
-      (switch-to-buffer project-buffer))))
 
 (defun arv-get-string-at-point ()
   "Retorna la cadena en el punt, ni si el punt no està sobre una
@@ -169,46 +83,42 @@ cadena."
                                   (1- (point)))))
         (buffer-substring-no-properties start end))))
 
-(defvar arv/django--app-models-cache nil
-  "llista associativa: app-label -> dades-model.")
+(defun arv/django--get-template-candidates (filename current-app)
+  "Return template candidates for `completing-read'.
 
-(defun arv/django--app-models-cache-populate-maybe (force)
-  ""
-  (if (or force
-          (null arv/django--app-models-cache))
-      (let ((apps-list (json-read-from-string
-                        (python-django-util-shell-command-or-error
-                         (format "%s -c \"%s%s\""
-                                 (executable-find python-shell-interpreter)
-                                 python-django-info-imports-code
-                                 (concat
-                                  "from django.apps import apps\n"
-                                  "models = {}\n"
-                                  "for app_label, model_dict in apps.all_models.items():\n"
-                                  "    app_models = []\n"
-                                  "    for model_name, model_class in model_dict.items():\n"
-                                  "        meta = model_class._meta\n"
-                                  "        app_models.append((\n"
-                                  "            app_label + '.' + meta.model_name,\n"
-                                  "            app_label + '.' + meta.object_name\n"
-                                  "        ))\n"
-                                  "    models[app_label] = app_models\n"
-                                  "print(json.dumps(models), end='')"))))))
-        (setq arv/django--app-models-cache
-              (mapcar (lambda (x) (cons (symbol-name (car x)) (arv/django--vector-to-list (cdr x))))
-                      apps-list))))
-  )
+FILENAME is the filename of a template, relative to the template
+directory ('admin/login.html'). CURRENT-APP is the label of some
+django app (usually the one to which the file we are editing
+belongs).
 
-(defun arv/django-get-app-models (app &optional force)
-  "Retorna el nom dels models definits per `app'."
-  (arv/django--app-models-cache-populate-maybe force)
-  (cdr (assoc app arv/django--app-models-cache)))
+This function returns a list of dotted pairs '(APP . FULL-PATH)'
+than can be feed to `completing-read'. The list contains actual
+templates matching FILENAME and, maybe, a fake one corresponding
+to CURRENT-APP."
+  (-non-nil
+   (mapcar
+    (lambda (app)
+      (let ((filename-full (f-join (cdr app) "templates" filename)))
+        (when (or (string= (car app) current-app)
+                  (file-exists-p filename-full))
+          (cons (car app) filename-full))))
+    (djira-info-get-all-apps-paths))))
 
-(defun arv/django-get-models (&optional force)
-  "Retorna el nom del tots els models."
-  (arv/django--app-models-cache-populate-maybe force)
-  (reduce 'append (mapcar (lambda (x) (arv/django-get-app-models x))
-                          (arv/django-get-app-names))))
+(defun arv/django--get-js-controller-candidates (filename current-app)
+  (-non-nil
+   (mapcar
+    (lambda (app)
+      (let ((filename-full (f-join (cdr app) "static" realname)))
+        (when (or (equal (car app) current-app)
+                  (file-exists-p filename-full))
+          (cons (car app) filename-full))))
+    (djira-info-get-all-apps-labels))))
+
+(defun arv/django--js-controller-to-filename (name)
+  (let ((parts (f-split name)))
+    (concat
+     (apply 'f-join (append (list (car parts) "js") (cdr parts)))
+     ".js")))
 
 ;;;###autoload
 (defun arv/django-jump-to-template ()
@@ -222,9 +132,6 @@ Cal que:
 * la ruta de la plantilla sigui relativa al directori 'templates'
   de l'aplicació (seguint el conveni django).
 
-* s'hagi carregat un projecte django per poder fer introspecció
-  de les aplicacions.
-
 La funció opera contruint una llista amb les aplicacions que
 contenen la plantilla. Per simplificar la creació de plantilles
 aquesta llista sempre contindrà el nom de l'aplicació
@@ -235,71 +142,35 @@ plantilla directament (creant-la si és necessari). Si conté més
 d'una aplicació permet triar quina obrir."
   (interactive)
   (let ((filename (arv-get-string-at-point))
-        (project-buffer (arv/django-get-project-buffer))
-        (current-app (arv/django-get-current-app))
-        (candidates ()))
-    (cond
-     ((null filename)
-      (message "Point must be over an string."))
-     ((null project-buffer)
-      (message "No open django project."))
-     (t
-      (progn
-        (with-current-buffer project-buffer
-          (dolist (app (python-django-info-get-app-paths))
-            (let ((filename-full
-                   (concat
-                    (file-name-as-directory (cdr app))
-                    (file-name-as-directory "templates")
-                    filename)))
-              (if (or (equal (car app) current-app)
-                      (file-exists-p filename-full))
-                  (setq candidates (cons (cons (symbol-name (car app)) filename-full) candidates))))))
+        (current-app (djira-get-app-for-buffer (current-buffer))))
+    (if (null filename)
+        (message "Point must be over an string.")
+      (let ((candidates (arv/django--get-template-candidates filename current-app)))
         (find-file (cdr (assoc
                          (if (= (length candidates) 1)
                              (caar candidates)
-                           (completing-read
-                            "Choose app: "
-                            candidates nil t nil))
-                         candidates))))))))
+                           (completing-read "Choose app: " candidates nil t nil))
+                         candidates)))))))
 
 ;;;###autoload
 (defun arv/django-jump-to-javascript-controller ()
-  ""
+  "Açò funciona amb el meu workflow.
+
+El controller és un identificador AMD, en el meu cas
+'app/controller'. Cal convertir-ho en 'app/js/controller.js'. per
+obtindre l'arxiu."
   (interactive)
-  (let ((filename (arv-get-string-at-point))
-        (realname)
-        (project-buffer (arv/django-get-project-buffer))
-        (current-app (arv/django-get-current-app))
-        (candidates ()))
-    (cond
-     ((null filename)
-      (message "Point must be over an string."))
-     ((null project-buffer)
-      (message "No open django project."))
-     (t
-      (progn
-        (setq realname (let ((parts (s-split "/" filename)))
-                         (concat
-                          (s-join "/" (append (list (car parts) "js") (cdr parts)))
-                          ".js")))
-        (with-current-buffer project-buffer
-          (dolist (app (python-django-info-get-app-paths))
-            (let ((filename-full
-                   (concat
-                    (file-name-as-directory (cdr app))
-                    (file-name-as-directory "static")
-                    realname)))
-              (if (or (equal (car app) current-app)
-                      (file-exists-p filename-full))
-                  (setq candidates (cons (cons (symbol-name (car app)) filename-full) candidates))))))
+  (let ((amd-name (arv-get-string-at-point)))
+    (if (null amd-name)
+        (message "Point must be over an string.")
+      (let* ((current-app (djira-get-app-for-buffer (current-buffer)))
+             (filename (arv/django--js-controller-to-filename amd-name))
+             (candidates (arv/django--get-js-controller-candidates filename current-app)))
         (find-file (cdr (assoc
                          (if (= (length candidates) 1)
                              (caar candidates)
-                           (completing-read
-                            "Choose app: "
-                            candidates nil t nil))
-                         candidates))))))))
+                           (completing-read "Choose app: " candidates nil t nil))
+                         candidates)))))))
 
 ;;;###autoload
 (defun arv/django-insert-template-name ()
@@ -327,10 +198,10 @@ buffer, sense extensió."
       (backward-char 2))))
 
 (defun arv/django--ido-select-app ()
-  (ido-completing-read "App: " (arv/django-get-app-names) nil t))
+  (ido-completing-read "App: " (djira-info-get-all-apps-labels) nil t))
 
 (defun arv/django--ido-select-model ()
-  (ido-completing-read "Model: " (mapcar 'cadr (arv/django-get-models)) nil t))
+  (ido-completing-read "Model: " (djira-info-get-all-apps-models) nil t))
 
 (defun arv/django-hera-notes ()
   "Executa `hera_notes'."
@@ -341,12 +212,13 @@ buffer, sense extensió."
 
 (defun arv/django--visit-file (dir-rel-path at-app-root)
   (let* ((app-name (arv/django--ido-select-app))
-         (app-root (arv/django--get-app-path app-name)))
+         (app-root (djira-info-get-app-path app-name)))
     (if at-app-root
         (setq app-root (file-name-directory app-root)))
     (setq app-root (concat app-root "/" dir-rel-path))
-    (ido-file-internal ido-default-file-method nil app-root))
-  )
+    (if (f-directory-p app-root)
+        (ido-file-internal ido-default-file-method nil app-root)
+      (find-file (concat app-root ".py")))))
 
 (defun arv/django-visit-app ()
   "Permet selecionar app i obrir un arxiu dins l'arrel de la app."
@@ -381,7 +253,7 @@ buffer, sense extensió."
 (defun arv/django-visit-project ()
   ""
   (interactive)
-  (ido-file-internal ido-default-file-method nil (arv/django-get-project-root)))
+  (ido-file-internal ido-default-file-method nil (djira-info-get-project-root)))
 
 ;; TODO: es pot navegar a la documentacions dels models en
 ;; http://localhost:8000/admin/docs/models
@@ -426,9 +298,6 @@ buffer, sense extensió."
   (define-key arv/django-mode-map (kbd "C-c d v t") 'arv/django-visit-app-test-module)
   (define-key arv/django-mode-map (kbd "C-c d v T") 'arv/django-visit-app-template-file)
   (define-key arv/django-mode-map (kbd "C-c d v v") 'arv/django-visit-app-view-module)
-  ;; project related
-  (define-key arv/django-mode-map (kbd "C-c d p") 'arv/django-switch-to-project-buffer)
-
 )
 
 (define-minor-mode arv/django-mode
